3258916df3f49b4519664233ae5c4fc3e4b3323b
Your Name <you@example.com>
2018-10-04 19:53:09 +0700

    Init

---
+++ AppVanMinh/Frames/Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppVanMinh.Frames
+{
+    public sealed partial class Login : ContentDialog
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Sign_Up(object sender, TappedRoutedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+            this.Hide();
+            rootFrame.Navigate(typeof(Sign));
+        }
+
+        private void Login_Handle(object sender, TappedRoutedEventArgs e)
+        {
+            var username = Username.Text;
+            var password = Password.Password.ToString();
+            if(username == "")
+            {
+                Username_Message.Text = "Username khong duoc de trong!";
+                Username_Message.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+            } else
+            {
+                Username_Message.Text = "";
+            }
+            if (password == "")
+            {
+                Password_Message.Text = "Mat khau khong duoc de trong!";
+                Password_Message.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+            } else
+            {
+                Password_Message.Text = "";
+            }
+            if(username != "" && password != "")
+            {
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ AppVanMinh/Frames/Sign.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppVanMinh.Frames
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Sign : Page
+    {
+        public Sign()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Handle_Signup(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Action success.");
+        }
+
+        private async void Capture_Photo(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private static async void GetUploadUrl()
+        {
+            
+        }
+
+        public async void HttpUploadFile(string url, string paramName, string contentType)
+        {
+            
+        }
+
+        private void Select_Gender(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioGender = sender as RadioButton;
+            //Debug.WriteLine(this.currentMember.gender);
+        }
+
+        private void Change_Birthday(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            //this.currentMember.birthday = sender.Date.Value.ToString("yyyy-MM-dd");
+        }
+
+        private void Sign_In(object sender, TappedRoutedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(MainPage));
+        }
+
+        private void Handle_Signup(object sender, TappedRoutedEventArgs e)
+        {
+            var email = Email.Text;
+            var password = Password.Password;
+            var confirm_password = Confirm_Password.Password;
+            var firstname = FirstName.Text;
+            var lastname = LastName.Text;
+            var avatar = ImageUrl.Text;
+            var phone = Phone.Text;
+            var address = Address.Text;
+            var intro = Introduction.Text;
+            var birthday = BirthDay.Date;
+        }
+    }
+}
